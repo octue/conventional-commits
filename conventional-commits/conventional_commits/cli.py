93e19133027dfab3ea3a942a1072a088c62ebc46
cortadocodes <cortado.codes@protonmail.com>
2021-06-07 16:19:25 +0100

    FIX: Parse commit message properly

--- conventional-commits/conventional_commits/cli.py
+++ conventional-commits/conventional_commits/cli.py
@@ -4,18 +4,18 @@ from conventional_commits.checker import ConventionalCommitMessageChecker
 
 
 def main(argv=None):
-    """
+    """Check if the git commit message adheres to the Conventional Commits standard and additional rules.
 
-    :param iter(str)|None argv:
-    :return int:
+    :param iter(str)|None argv: iterable containing single argument, which should be the path to a git commit message
+    :return int: the return code - 0 if the message passes, 1 if it fails
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('commit_message_path', type=str, help='Path to current git commit message.')
 
     args = parser.parse_args(argv)
 
-    with open(args[0]) as f:
-        commit_message_lines = f.readlines()
+    with open(args.commit_message_path) as f:
+        commit_message_lines = f.read().splitlines()
 
     try:
         ConventionalCommitMessageChecker().check_commit_message(commit_message_lines)
