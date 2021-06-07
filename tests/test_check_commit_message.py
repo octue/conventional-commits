9b2f6daba04cc769462d5ccf3c88f70f5805dbc4
cortadocodes <cortado.codes@protonmail.com>
2021-06-07 21:10:54 +0100

    TST: Document tests

--- tests/test_check_commit_message.py
+++ tests/test_check_commit_message.py
@@ -5,49 +5,61 @@ from pre_commit_hooks.conventional_commits.checker import ConventionalCommitMess
 
 class TestCheckCommitMessage(unittest.TestCase):
     def test_empty_commit_message_raises_error(self):
+        """Test that an empty commit message results in an error."""
         with self.assertRaises(ValueError):
             ConventionalCommitMessageChecker().check_commit_message([])
 
     def test_header_but_no_body_when_body_is_required_raises_error(self):
+        """Test that a commit message with a header but no body when the body is required results in an error."""
         with self.assertRaises(ValueError):
             ConventionalCommitMessageChecker(require_body=True).check_commit_message(["FIX: Fix this bug"])
 
     def test_with_header_but_no_body_when_body_is_not_required(self):
+        """Test that a commit message with a header but no body when the body is not required results is ok."""
         ConventionalCommitMessageChecker().check_commit_message(["FIX: Fix this bug"])
 
     def test_empty_header_raises_error(self):
+        """Test that a commit message with an empty header results in an error."""
         with self.assertRaises(ValueError):
             ConventionalCommitMessageChecker().check_commit_message([""])
 
     def test_invalid_commit_code_raises_error(self):
+        """Test that a commit message with an invalid commit code results in an error."""
         with self.assertRaises(ValueError):
             ConventionalCommitMessageChecker().check_commit_message(["BLAH: This is a blah commit"])
 
     def test_header_over_maximum_length_raises_error(self):
+        """Test that a commit message with a header that is too long results in an error."""
         with self.assertRaises(ValueError):
             ConventionalCommitMessageChecker().check_commit_message([f"IMP: {'a' * 80}"])
 
     def test_invalid_header_ending_raises_error(self):
+        """Test that a commit message with a header with an invalid ending results in an error."""
         with self.assertRaises(ValueError):
             ConventionalCommitMessageChecker().check_commit_message(["FIX: Fix this bug."])
 
     def test_non_blank_header_separator_line_raises_error(self):
+        """Test that a commit message with a non-blank header separator line results in an error."""
         with self.assertRaises(ValueError):
             ConventionalCommitMessageChecker().check_commit_message(["FIX: Fix this bug", "Body started too early."])
 
     def test_with_body_when_body_not_required(self):
+        """Test that a commit message with a valid header and body when a body is not required is ok."""
         ConventionalCommitMessageChecker().check_commit_message(["FIX: Fix this bug", "", "This is the body."])
 
     def test_with_body_when_body_required(self):
+        """Test that a commit message with a valid header and body when a body is required is ok."""
         ConventionalCommitMessageChecker(require_body=True).check_commit_message(
             ["FIX: Fix this bug", "", "This is the body."]
         )
 
     def test_empty_body_raises_error(self):
+        """Test that a commit message with an empty body results in an error."""
         with self.assertRaises(ValueError):
             ConventionalCommitMessageChecker(require_body=True).check_commit_message(["FIX: Fix this bug", "", ""])
 
-    def test_body_lines_over_maximum_lenth_raises_error(self):
+    def test_body_lines_over_maximum_length_raises_error(self):
+        """Test that a commit message with a body that has a line that is too long results in an error."""
         with self.assertRaises(ValueError):
             ConventionalCommitMessageChecker(require_body=True).check_commit_message(
                 ["FIX: Fix this bug", "", f"{'a' * 80}"]
@@ -59,6 +71,7 @@ class TestCheckCommitMessage(unittest.TestCase):
             )
 
     def test_comments_lines_are_ignored(self):
+        """Test that comment lines in the commit message are ignored."""
         ConventionalCommitMessageChecker().check_commit_message(
             [
                 "FIX: Fix this bug",
@@ -77,4 +90,5 @@ class TestCheckCommitMessage(unittest.TestCase):
         )
 
     def test_trailing_newline_is_ignored(self):
+        """Test that a trailing newline in the commit message is ignored."""
         ConventionalCommitMessageChecker().check_commit_message(["FIX: Fix this bug", ""])
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,12 +6,14 @@ from pre_commit_hooks.conventional_commits.cli import main
 
 class TestCLI(unittest.TestCase):
     def test_main_with_valid_commit_message(self):
+        """Test that the entrypoint works with a valid commit message."""
         with patch("pre_commit_hooks.conventional_commits.cli.open", mock_open(read_data="FIX: Fix a bug")):
             return_code = main()
 
         self.assertEqual(return_code, 0)
 
     def test_main_with_invalid_commit_message(self):
+        """Test that an error is raised by the entrypoint when an invalid commit message is given."""
         with patch("pre_commit_hooks.conventional_commits.cli.open", mock_open(read_data="blah\nawful commit message")):
             return_code = main()
 
