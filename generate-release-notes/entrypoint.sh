3ffe073bdffa7fa6fc5a0c10de61f3d53908c99d
cortadocodes <cortado.codes@protonmail.com>
2022-10-03 12:47:52 +0100

    FIX: Add missing argument in entrypoint

--- generate-release-notes/entrypoint.sh
+++ generate-release-notes/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 cd $GITHUB_WORKSPACE
-echo "::set-output name=release_notes::$(compile-release-notes $1 $2 $3 $4 $5)"
+echo "::set-output name=release_notes::$(compile-release-notes PULL_REQUEST_START $1 $2 $3 $4 $5)"
